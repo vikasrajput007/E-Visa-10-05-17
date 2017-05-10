66fe679ab52255d568d2f3670dd5eb576f0b3a9d
HP <vicay0001@gmail.com>
2017-05-10 18:33:55 +0530

    spinner is also added in the destination page

--- app/src/main/java/com/android/developer/e_visa/DestinationCountryActivity.java
+++ app/src/main/java/com/android/developer/e_visa/DestinationCountryActivity.java
@@ -6,15 +6,19 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class DestinationCountryActivity extends AppCompatActivity {
 
      RelativeLayout button;
     ImageView apply_now;
+    Spinner spinner,spinner2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +26,38 @@ public class DestinationCountryActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        spinner = (Spinner) findViewById(R.id.simpleSpinner);
+        spinner2 = (Spinner) findViewById(R.id.simpleSpinner2);
         button = (RelativeLayout)findViewById(R.id.apply_now);
+
+        spinner.setOnItemSelectedListener(new DestinationCountryActivity.ItemSelectedListener());
 
 
 
     }
+
+    public class ItemSelectedListener implements AdapterView.OnItemSelectedListener {
+
+        //get strings of first item
+        String firstItem = String.valueOf(spinner.getSelectedItem());
+
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+            if (firstItem.equals(String.valueOf(spinner.getSelectedItem()))) {
+                // ToDo when first item is selected
+            } else {
+                Toast.makeText(parent.getContext(),
+                        "You have selected : " + parent.getItemAtPosition(pos).toString(),
+                        Toast.LENGTH_LONG).show();
+                // Todo when item is selected by the user
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> arg) {
+
+        }
+
+    }
+
 
 }
